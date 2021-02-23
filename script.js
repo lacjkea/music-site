56a299d021de3799dc2e08ff43b25ff0860bf4c2
lacj@kea.dk <lacj@kea.dk>
2021-02-23 23:18:22 +0100

    checkboxes, filters & new fechiies working

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+//https://s2021-8556.restdb.io/rest/great-songs?apikey=6034ed655ad3610fb5bb655d
+//https://s2021-8556.restdb.io/rest/great-songs?q={%22artist%22:%22BABYMETAL%22}&apikey=6034ed655ad3610fb5bb655d
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const product = urlParams.get("product");
+
+const mainEl = document.querySelector("main");
+
 //alert("hey");
 // end point
-const url = "https://s2021-8556.restdb.io/rest/great-songs?max=20";
+const endpointurl = "https://s2021-8556.restdb.io/rest/great-songs?max=20";
 
 const mediaurl = "https://s2021-8556.restdb.io/media/";
 
@@ -10,7 +19,73 @@ const options = {
   },
 };
 
-function getData() {
+//https://s2021-8556.restdb.io/rest/great-songs/_meta?apikey=6034ed655ad3610fb5bb655d
+
+function getNav() {
+  fetch("https://s2021-8556.restdb.io/rest/great-songs/_meta", options)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      //We have the data
+      //console.log(data);
+      buildFilters(data);
+    })
+    .catch((e) => {
+      //Woops, something went wrong
+      console.error("An error occured:", e.message);
+    });
+  getData(endpointurl);
+}
+
+function buildFilters(data) {
+  console.log("hi");
+  // console.log(data.fields[10].properties.option_list);
+  const genres = data.fields[10].properties.option_list.split(", ");
+  genres.forEach((genre) => {
+    const template = document.querySelector(".genre").content;
+    const clone = template.cloneNode(true);
+    const inputEl = clone.querySelector("input");
+    inputEl.name = genre;
+    inputEl.id = genre;
+    inputEl.addEventListener("change", reLoad);
+    const formEl = document.querySelector("form");
+    const labelEl = clone.querySelector("label");
+    labelEl.setAttribute("for", genre);
+    labelEl.textContent = genre;
+
+    formEl.appendChild(clone);
+
+    function reLoad(evt) {
+      const listChecked = formEl.querySelectorAll("input:checked");
+      let listGenresChecked = [];
+      console.log(listChecked);
+      listChecked.forEach((genreChosen) => {
+        listGenresChecked.push('"' + genreChosen.id + '"');
+      });
+      //listGenresChecked.;
+      //console.log(listGenresChecked.toString());
+
+      const genresCheckedString = listGenresChecked.toString();
+      console.log(genresCheckedString);
+      const newUrl =
+        'https://s2021-8556.restdb.io/rest/great-songs?q={"genres":{"$in":[' +
+        genresCheckedString +
+        "]}}";
+      console.log(newUrl);
+      mainEl.innerHTML = "";
+      document.querySelector(".preloader").classList.remove("hide");
+      getData(newUrl);
+    }
+
+    //
+  });
+}
+
+function getData(url) {
   fetch(url, options)
     .then((response) => {
       if (!response.ok) {
@@ -20,21 +95,21 @@ function getData() {
     })
     .then((data) => {
       //We have the data
-      console.log(data);
+      //console.log(data);
       show(data);
     })
     .catch((e) => {
       //Woops, something went wrong
       console.error("An error occured:", e.message);
     });
-  document.querySelectorAll("input").forEach((box) => {
-    box.addEventListener(click, hey);
-  });
+  // document.querySelectorAll("input").forEach((box) => {
+  //   box.addEventListener("change", hey);
+  // });
 }
 
-/* function box() {
-  alert("hey");
-} */
+// function hey() {
+//   alert("hey");
+// }
 /* async function getData() {
   const response = await fetch(url, options);
   const data = await response.json();
@@ -45,7 +120,7 @@ function getData() {
 function show(data) {
   console.table(data);
   data.forEach((song) => {
-    const template = document.querySelector("template").content;
+    const template = document.querySelector(".song").content;
     const clone = template.cloneNode(true);
 
     clone.querySelector("h2").textContent = song.title;
@@ -59,6 +134,12 @@ function show(data) {
     const ytLink = "https://www.youtube.com/embed/" + song.youtube_embed;
     clone.querySelector("iframe").src = ytLink;
 
+    const img = clone.querySelector("img");
+    img.src = song.img;
+    img.alt = song.artist;
+    clone.querySelector("figcaption").innerHTML =
+      "Image attribution: " + song.img_attribution;
+
     const genres = song.genres;
     //https://www.w3schools.com/jsref/jsref_split.asp
     const ulEl = clone.querySelector("ul");
@@ -71,11 +152,10 @@ function show(data) {
     });
 
     // clone.querySelector(".preloader").remove();
-    const mainEl = document.querySelector("main");
     mainEl.appendChild(clone);
   });
-  document.querySelector(".preloader").remove();
+  document.querySelector(".preloader").classList.add("hide");
 }
 
 //getData();
-window.addEventListener("DOMContentLoaded", getData);
+window.addEventListener("DOMContentLoaded", getNav);
